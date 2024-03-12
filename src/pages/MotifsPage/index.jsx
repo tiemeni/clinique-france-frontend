d82964f450dcd975510404777b2bac35dd375cc5
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2024-03-12 15:48:03 +0100

    fix search motif (backend-less) the server still does not return the correct data during a search

--- src/pages/MotifsPage/index.jsx
+++ src/pages/MotifsPage/index.jsx
@@ -7,16 +7,19 @@ import RessourceSearchLayout from '../../layouts/RessourceSearchLayout';
 import { motif } from '../../utils/data';
 import { dataMotifs } from '../../utils/dataFields';
 import TableGenerator from '../../layouts/TableGenerator';
-import { deleteMotif, getAllMotifs,searchMotif  } from '../../redux/motifs/actions';
+import { deleteMotif, getAllMotifs,/* searchMotif */ } from '../../redux/motifs/actions';
 import { getAllConsignes } from '../../redux/consignes/actions';
+import { SEARCH_WB_MOTIF } from '../../redux/motifs/types';
 
 function MotifsPage() {
   const dispatch = useDispatch();
   const motifs = useSelector((state) => state.Motifs.motifs);
+  const filteredMotifs = useSelector((state) => state.Motifs.filteredMotifs);
 
   useEffect(() => {
     dispatch(getAllMotifs());
     dispatch(getAllConsignes());
+
   }, []);
 
   const handleDeleteEntity = (id) => {
@@ -27,18 +30,32 @@ function MotifsPage() {
  const type='MOTIF';
 
   const handlePost = (m) => {
+    
     if(m !== null ){
-      dispatch(searchMotif({nom : m?.nom, couleur: m?.couleur}));
+      //  dispatch(searchMotif({nom : m?.nom, couleur: m?.couleur}));
+      dispatch({type: SEARCH_WB_MOTIF, payload:m.nom?.toLowerCase()})
     }
   };
 
+  const numberOfMotifsLabel = () => {
  
+      if (filteredMotifs?.length === 0) {
+      return   `aucun motif ne correspond à votre recherche`;
+    }
+    if (filteredMotifs.length < motifs.length) {
+      return  `${filteredMotifs?.length} motif${filteredMotifs?.length===1? '':'(s)'} sur ${motifs?.length} correspond${filteredMotifs?.length===1? '':'ent'} à votre recherche`;
+    } 
+
+    return `${filteredMotifs?.length} motif${filteredMotifs?.length===1? '':'(s)'} au total`;
+  
+   
+ }
 
   return (
     <Box p={5} spacing={5}>
       <RessourceSearchLayout data={motif} type={type} cle={cle} handlePost={handlePost} />
       <p style={{ marginTop: 15 }}>
-        {motifs?.length} motifs correspondent a votre recherche
+        {numberOfMotifsLabel()}
       </p>
       <Link to="upsert">
         <Button
