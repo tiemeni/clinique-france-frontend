f80dbbb11743760540cb2a4f2a027b514882868e
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2024-06-06 15:13:29 +0100

    ajust verify token overlay background

--- src/pages/VerifyTokenPage/index.jsx
+++ src/pages/VerifyTokenPage/index.jsx
@@ -1,13 +1,19 @@
 import React from "react"
+import Color from 'color'
+
+// rgba(52, 58, 64, 0.3) 
 
 function VerifyTokenPage() {
    
   return (
-      <div className="verify-token-modal" style={{ height: '100vh', width: '100vw', position:'absolute', backgroundColor: 'rgba(0,0,0,0.5)', display: 'flex', justifyContent:'center', alignItems:'center',zIndex:100000000 }}>
+   
+      <div className="verify-token-modal" style={{ height: '100vh', width: '100vw', position:'absolute', backgroundColor: Color('#1976d2').darken(0.5).alpha(0.6).toString(),right:0, left:0, top:0, display: 'flex', justifyContent:'center', alignItems:'center',zIndex:100000000 }}>
           <div>
-            <h1 style={{color:'white'}}>Vérification du token... </h1>
+            <h1 style={{color:'white', fontWeight:'bold', fontSize:'24px'}}>Vérification du token... </h1>
           </div>
       </div>
+  
+      
   )
 }
 
